53d978caa1dd1d2eac83f6df2afb31e241938946
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-08-28 09:02:28 +0700

    Update

---
+++ src/auth/passport.ts
@@ -0,0 +1,28 @@
+import passportIns from "passport";
+import { dbClient } from "@db/client.js";
+import { eq } from "drizzle-orm";
+import { usersTable } from "@db/schema.js";
+import { local } from "./passportLocal.js";
+import { github } from "./passportOauthGithub.js";
+import { google } from "./passportOauthGoogle.js";
+
+passportIns.use(local);
+passportIns.use("github", github);
+passportIns.use("google", google);
+
+passportIns.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passportIns.deserializeUser<string>(async function (id, done) {
+  const query = await dbClient.query.usersTable.findFirst({
+    where: eq(usersTable.id, id),
+  });
+  if (!query) {
+    done(null, false);
+  } else {
+    done(null, query);
+  }
+});
+
+export default passportIns;
